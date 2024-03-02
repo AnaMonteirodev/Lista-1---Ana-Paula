1ad40d57f81d84170875ab886a9aee9f12f1e9b0
Ana Paula Monteiro <151549778+AnaMonteirodev@users.noreply.github.com>
2024-03-02 07:57:43 -0300

    Add files via upload

---
+++ questao20.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("Inteiro equivalente ao caractere 'A': %d\n", 'A');
+    printf("Inteiro equivalente ao caractere 'B': %d\n", 'B');
+    printf("Inteiro equivalente ao caractere 'C': %d\n", 'C');
+    printf("Inteiro equivalente ao caractere 'a': %d\n", 'a');
+    printf("Inteiro equivalente ao caractere 'b': %d\n", 'b');
+    printf("Inteiro equivalente ao caractere 'c': %d\n", 'c');
+    printf("Inteiro equivalente ao caractere '0': %d\n", '0');
+    printf("Inteiro equivalente ao caractere '1': %d\n", '1');
+    printf("Inteiro equivalente ao caractere '2': %d\n", '2');
+    printf("Inteiro equivalente ao caractere '$': %d\n", '$');
+    printf("Inteiro equivalente ao caractere '*': %d\n", '*');
+    printf("Inteiro equivalente ao caractere '+': %d\n", '+');
+    printf("Inteiro equivalente ao caractere '/': %d\n", '/');
+    printf("Inteiro equivalente ao caractere ' ': %d\n", ' ');
+
+    return 0;
+}
